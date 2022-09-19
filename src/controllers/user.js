6cf863fce162e352a61f3836b8236d143f31ee29
Omar Alsyouf <omaralsoyouf@gmail.com>
2022-09-19 17:44:06 +0100

    create user endpoint

--- src/controllers/user.js
+++ src/controllers/user.js
@@ -1,8 +1,55 @@
+const prisma = require('../utils/prisma');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+const Joi = require('joi');
+
+const key = process.env.KEY;
+const saltRounds = 10;
+
 const createUser = async (req, res) => {
   try {
-    res.json({ data: 'Hello I am the new user!' });
-  } catch (err) {
-    return res.json({ err: err.message });
+    const schema = Joi.object({
+      firstname: Joi.string().min(2).max(15).required(),
+      lastname: Joi.string().min(2).max(15).required(),
+      email: Joi.string()
+        .email({
+          minDomainSegments: 2,
+          tlds: { allow: ['com', 'net'] },
+        })
+        .required(),
+      password: Joi.string().min(3).max(30).required(),
+      confirmPass: Joi.ref('password'),
+    });
+
+    const { error, value } = schema.validate(req.body);
+    if (error) {
+      res.status(400);
+      res.json({ error: error.details[0].message });
+      return;
+    }
+    const { password, email, firstname, lastname } = value;
+    const userByEmail = await prisma.user.findUnique({ where: { email } });
+
+    if (userByEmail) {
+      res.status(409);
+
+      res.json({ error: 'This Email is already exists' });
+      return;
+    }
+
+    const hash = await bcrypt.hash(password, saltRounds);
+    const createdUser = await prisma.user.create({
+      data: {
+        firstname,
+        lastname,
+        email,
+        password: hash,
+      },
+    });
+    const token = jwt.sign({ id: createdUser.id }, key);
+    res.json({ data: createdUser, token });
+  } catch (e) {
+    return res.json({ err: e.message });
   }
 };
 
